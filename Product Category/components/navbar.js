28a6b972265fc8d0a9bd26c10525fcf0bb92a34c
gkm8078 <kumar102299madan@gmail.com>
2022-04-02 04:14:10 +0530

    add product category file

---
+++ Product Category/components/navbar.js
@@ -0,0 +1,10 @@
+
+const navbar = ()=> {
+
+    return `<div id="navbar-nail-art">
+                <img src="https://cdn2.stylecraze.com/wp-content/uploads/static-content/logos/sc-logo.png.webp" alt="logo image">
+            </div>`
+
+}
+
+export default navbar
---
+++ Product Category/scripts/main.js
@@ -0,0 +1,8 @@
+
+// home page codes start
+document.querySelector("#home").addEventListener("click", ()=>{
+
+    window.location.href = "#"
+
+})
+// home page codes ends
